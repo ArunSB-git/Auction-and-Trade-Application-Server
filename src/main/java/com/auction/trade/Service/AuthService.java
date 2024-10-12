852df1cf89990cc606b4876e8bab422d700193b1
arun-newstreettech <arunatnewstreettech@gmail.com>
2024-10-12 23:09:04 +0530

    Login API

--- src/main/java/com/auction/trade/Service/AuthService.java
+++ src/main/java/com/auction/trade/Service/AuthService.java
@@ -1,11 +1,13 @@
 package com.auction.trade.Service;
 
 import com.auction.trade.DTO.AuthRepository;
+import com.auction.trade.LoginResponse;
 import com.auction.trade.Models.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthService {
@@ -27,4 +29,14 @@ public class AuthService {
 //    public List<Users> getAllByNationality(String nationality) {
 //        return authRepository.findByNationality(nationality);
 //    }
+
+    public Optional<LoginResponse> login(String username, String password) {
+        Optional<Users> user = authRepository.findByUsername(username);
+        if (user.isPresent() && user.get().getPassword().equals(password)) {
+            Users foundUser = user.get();
+            return Optional.of(new LoginResponse(foundUser.getUserid(), foundUser.getRole()));
+        }
+        return Optional.empty();
+    }
+
 }
